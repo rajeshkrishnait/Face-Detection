4dd01848dfb2ddb3cf35bc65295bb8b6f01d5776
rajeshkrishnait <rajeshkrishnarajakumar@gmail.com>
2021-05-14 01:03:54 +0530

    deleted images

--- faceDetection.py
+++ faceDetection.py
@@ -1,21 +1,23 @@
 import cv2
 from random import randrange
+#press q to quit webcam
 trained_face_data=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# img=cv2.imread('det2.jpeg')
 webcam=cv2.VideoCapture(0)
 
 while True:
     successful_frame_read, frame=webcam.read()
     greyscaled_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    face_coordinates=trained_face_data.detectMultiScale(greyscaled_frame)
-    # print(face_coordinates)
-    # for i in face_coordinates:
-    #      cv2.rectangle(frame,(i[0],i[1]),(i[0]+i[2],i[3]+i[1]),(255,0,0),4)
+    face_coordinates=trained_face_data.detectMultiScale(greyscaled_frame,scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30))
+    print(face_coordinates)
+    for i in face_coordinates:
+         cv2.rectangle(frame,(i[0],i[1]),(i[0]+i[2],i[3]+i[1]),(255,0,0),4)
     cv2.imshow('sadf',frame)
     print(face_coordinates)
-    if len(face_coordinates)>1:
-        print("Another person detected")
-        break
+    # if len(face_coordinates)>1:
+    #     print("Another person detected")
+    #     break
     key=cv2.waitKey(1)
     if key==81 or key==113:
         break
